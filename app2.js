633a1f005e586b62a85af6ce6b049140d6ca8b4c
Tachobo <wilmerardila2001@gmail.com>
2025-10-06 20:54:25 -0500

    Segundo commit: lo que alcance a hacer en clase

---
+++ app2.js
@@ -0,0 +1,5 @@
+alert(`En este programa se clasificara la velocidad de un vehiculo
+en muy lento, velocidad moderada, rapido y muy rapido
+dependiendo de la velocidad digitada por el usuario`);
+
+let velocidad = parseFloat(prompt());
